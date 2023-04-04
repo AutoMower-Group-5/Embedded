7d8cc21475bd6db693cc7c26ce4730af49fc080a
Axel Björkman <bjax20rb>
2023-04-04 16:30:36 +0200

    First trial of rotating. Will try to investigate the gyroscope

--- automower/automower.ino
+++ automower/automower.ino
@@ -1,67 +1,76 @@
 #include <MeAuriga.h>
 #include <SoftwareSerial.h>
 
-unsigned char table[128] = {0};
-SoftwareSerial softuart(13,12);
+unsigned char table[128] = { 0 };
+SoftwareSerial softuart(13, 12);
 MeSerial mySerial(PORT_8);
+MeUltrasonicSensor ultraSensor(PORT_7); /* Ultrasonic module can ONLY be connected to port 3, 4, 6, 7, 8 of base shield. */
 
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 int16_t moveSpeed = 200;
 
-void Forward(void)
-{
-  Encoder_1.setMotorPwm(-moveSpeed); // setMotorPwm writes to the encoder controller
-  Encoder_2.setMotorPwm(moveSpeed);  // so setting the speed change instantly
+
+void SetMotors(float motor1Percent, float motor2Percent) {
+  Encoder_1.setMotorPwm((motor1Percent / 100.0) * moveSpeed);
+  Encoder_2.setMotorPwm((motor2Percent / 100.0) * moveSpeed);
 }
-void Backward(void)
-{
+
+void Forward(void) {
+  Encoder_1.setMotorPwm(-moveSpeed);  // setMotorPwm writes to the encoder controller
+  Encoder_2.setMotorPwm(moveSpeed);   // so setting the speed change instantly
+}
+
+void Backward(void) {
   Encoder_1.setMotorPwm(moveSpeed);
   Encoder_2.setMotorPwm(-moveSpeed);
 }
-void BackwardAndTurnLeft(void)
-{
-  Encoder_1.setMotorPwm(moveSpeed/4);
+void BackwardAndTurnLeft(void) {
+  Encoder_1.setMotorPwm(moveSpeed / 4);
   Encoder_2.setMotorPwm(-moveSpeed);
 }
-void BackwardAndTurnRight(void)
-{
+void BackwardAndTurnRight(void) {
   Encoder_1.setMotorPwm(moveSpeed);
-  Encoder_2.setMotorPwm(-moveSpeed/4);
+  Encoder_2.setMotorPwm(-moveSpeed / 4);
 }
-void TurnLeft(void)
-{
+void TurnLeft(void) {
   Encoder_1.setMotorPwm(-moveSpeed);
-  Encoder_2.setMotorPwm(moveSpeed/2);
+  Encoder_2.setMotorPwm(moveSpeed / 2);
 }
-void TurnRight(void)
-{
-  Encoder_1.setMotorPwm(-moveSpeed/2);
+void TurnRight(void) {
+  Encoder_1.setMotorPwm(-moveSpeed / 2);
   Encoder_2.setMotorPwm(moveSpeed);
 }
-void TurnLeft1(void)
-{
+void TurnLeft1(void) {
   Encoder_1.setMotorPwm(-moveSpeed);
   Encoder_2.setMotorPwm(-moveSpeed);
 }
-void TurnRight1(void)
-{
+void TurnRight1(void) {
   Encoder_1.setMotorPwm(moveSpeed);
   Encoder_2.setMotorPwm(moveSpeed);
 }
-void Stop(void)
-{
+void Stop(void) {
   Encoder_1.setMotorPwm(0);
   Encoder_2.setMotorPwm(0);
 }
-void ChangeSpeed(int16_t spd)
-{
+void ChangeSpeed(int16_t spd) {
   moveSpeed = spd;
 }
-void setup()
-{
+
+void Rotate(int16_t deg, int clockwise) {
+  ChangeSpeed(100);
+  for (int i = 0; i < deg; i++) {
+    TurnRight1();
+    delay(10);
+  }
+}
+
+
+
+
+void setup() {
   Serial.begin(115200);
- 
+
   //Set PWM 8KHz
   TCCR1A = _BV(WGM10);
   TCCR1B = _BV(CS11) | _BV(WGM12);
@@ -69,39 +78,73 @@ void setup()
   TCCR2B = _BV(CS21);
 }
 
-void loop()
-{
-  //ChangeSpeed(100);
-  //Forward();
-  //delay(500);
-  //Backward();
-  //delay(500);
-  //TurnLeft1();
-  //delay(500);
-  //TurnRight1();
-  //delay(500);
-  //Stop();
-  //delay(1000);
-  if(Serial.available())
-  {
-    char rec_str = Serial.read();
-    delay(1);
-    Serial.println(rec_str);
+enum Mode { Manual,
+            Auto };
+enum Mode MODE = Auto;
 
-    switch(rec_str)
-    {
-      case 'w':
+void loop() {
+
+  switch (MODE) {
+    case Manual:
       {
-        Forward();
-        delay(1);
-        break;
+
+        if (Serial.available()) {
+          char rec_str = Serial.read();
+          switch (rec_str) {
+            case 'w':
+              {
+                Forward();
+                break;
+              }
+            case 's':
+              {
+                Backward();
+                break;
+              }
+            case 'a':
+              {
+                TurnLeft();
+                break;
+              }
+            case 'd':
+              {
+                TurnRight();
+                break;
+              }
+            case 'q':
+              {
+                Stop();
+                break;
+              }
+          }
+        }
       }
-      case ' ':
+    case Auto:
       {
-        Stop();
-        delay(1);
+
+        enum state_AutoMower { Locate_Path,
+                               Forward_Fast,
+                               Forward_Approach,
+                               Colission
+        };
+
+        enum state_AutoMower state = Locate_Path;
+        int distance_cm = ultraSensor.distanceCm();
+        ChangeSpeed(255);
+
+        switch (state) {
+
+          case Locate_Path:
+            {
+              Rotate(90, 0);
+            }
+
+          default:
+            state = Locate_Path;
+            break;
+        }
       }
-    }
+    default:
+      break;
   }
-
 }
