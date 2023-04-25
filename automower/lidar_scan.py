da562710ee66ab00769609b1108136012d272058
Jakoob Ishak <jakoob_ishak@hotmail.com>
2023-04-25 18:38:37 +0200

    Checks if the mower is in auto or manual mode

--- automower/lidar_scan.py
+++ automower/lidar_scan.py
@@ -3,9 +3,13 @@ import time
 import threading
 import serial
 import queue
+import importlib
+
+bt_script = importlib.import_module("bt_com")
 
 LIDAR_MIN_ANGLE = 170
 LIDAR_MAX_ANGLE = 190
+bt_msg = "M:A"
 
 def lidar_scan_data(stop_flag, scan_queue):
     print("Scanning data...")
@@ -21,12 +25,12 @@ def lidar_scan_data(stop_flag, scan_queue):
  
 def process_scan_data(stop_flag, scan_queue):
     global LIDAR_MIN_ANGLE
-    global LIDAR_MAX_ANGLE 
+    global LIDAR_MAX_ANGLE
+    global bt_msg
+    
     while not stop_flag.is_set():
         try:
             scan_data = scan_queue.get(block=True)
-            #lidar_min_angle = 170 # Min angle for the lidar to detect an object
-            #lidar_max_angle = 190 # Max angle for the lidar to detect an object
             lidar_min_dist = 200  # Min distance for the lidar to detect an object in mm
             # Process the scan data here
             for point in scan_data:
@@ -37,7 +41,7 @@ def process_scan_data(stop_flag, scan_queue):
                 print(quality, angle, distance)
                 if (quality > 13 and
                     LIDAR_MIN_ANGLE <= angle <= LIDAR_MAX_ANGLE
-                    and distance < lidar_min_dist):
+                    and distance < lidar_min_dist and bt_msg == "M:A"):
                     write_to_arduino(angle, scan_queue)
                     break
             
@@ -50,7 +54,8 @@ def process_scan_data(stop_flag, scan_queue):
 # TODO: Clean up the code and bugs
 def write_to_arduino(angle, scan_queue):
     global LIDAR_MIN_ANGLE
-    global LIDAR_MAX_ANGLE 
+    global LIDAR_MAX_ANGLE
+
     # Object detected, sending stop command to the arduino
     data_send = "L:DET\n"
     ser.write(data_send.encode())
@@ -94,9 +99,11 @@ def write_to_arduino(angle, scan_queue):
             
             with scan_queue.mutex:
                 scan_queue.queue.clear()
- 
+def run_bt_com():
+    bt_script.run_bt_com()
+
 if __name__ == "__main__":
-    serial_port = "/dev/ttyUSB1" # Serial port for the arduino
+    serial_port = "/tmp/vp1" # Serial port for the arduino
     serial_baud_rate = 9600  # Baud rate for the arduino
     ser = serial.Serial(serial_port, serial_baud_rate) # Init serial
     
@@ -105,20 +112,24 @@ if __name__ == "__main__":
  
     stop_flag = threading.Event()
     scan_queue = queue.Queue()
- 
+    
     scan_thread = threading.Thread(target=lidar_scan_data, args=(stop_flag, scan_queue))
     process_thread = threading.Thread(target=process_scan_data, args=(stop_flag, scan_queue))
+    bt_thread = threading.Thread(target=run_bt_com)
  
     scan_thread.start()
     process_thread.start()
- 
+    bt_thread.start()
+    
     try:
         while True:
+            bt_msg = bt_script.data
             time.sleep(1)
     except KeyboardInterrupt:
         stop_flag.set()
         scan_thread.join()
         process_thread.join()
+        bt_thread.join()
         lidar.stop()
         lidar.disconnect()
         print("Stopped scanning")
